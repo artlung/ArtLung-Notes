fa59592412464645339d3682b9bacfe924fb0f04
Joe Crawford <joe@artlung.com>
2010-06-23 18:58:07 -0400

    sizing commented out

--- v2/index.php
+++ v2/index.php
@@ -19,7 +19,7 @@ if ($handle = opendir('notes/')) {
     while (false !== ($file = readdir($handle))) {
         $item = json_decode(file_get_contents('notes/' . $file), true);
 		if ($item) {
-			print "<div class=\"instance savedNote\" id=\"{$item['guid']}\" style=\"top:{$item['top']}px;left:{$item['left']}px;height:{$item['height']}px;\">";
+			print "<div class=\"instance savedNote\" id=\"{$item['guid']}\" style=\"top:{$item['top']}px;left:{$item['left']}px;/*height:{$item['height']}px;*/\">";
 			print htmlentities($item['content']);
 			print "</div>\n";
 		}
