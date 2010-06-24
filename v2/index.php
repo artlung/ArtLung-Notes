db4d9dda8063a4c9eddbda90ce08c06c822c46c0
Joe Crawford <joe@artlung.com>
2010-06-24 14:29:21 -0400

    added commented out references to load.php file

--- v2/index.php
+++ v2/index.php
@@ -1,20 +1,21 @@
 <html>
 <head>
 <title>ArtLung Notes v2</title>
-<!-- Sencha CSS -->
 <link rel="stylesheet" href="sencha/resources/css/ext-touch.css" type="text/css">
-<!-- Custom CSS -->
 <link rel="stylesheet" href="css/style.css" type="text/css">
-<!-- Ext Touch JS -->
 <script type="text/javascript" src="sencha/ext-touch-debug.js"> </script>
-<!-- Application JS -->
 <script type="text/javascript" src="js/index.js"></script>
 </head>
 <body>
 <div id="everything">
-<script>
+<script type="text/javascript">
 	document.write(STATE.getSavedNotes());
 </script>
+<?php
+/*
+include('load.php');
+*/
+?>
 </div>
 </body>
 </html>
