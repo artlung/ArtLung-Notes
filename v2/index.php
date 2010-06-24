cb0c0166692913a836b09bbe716f8e905721e683
Joe Crawford <joe@artlung.com>
2010-06-24 14:27:24 -0400

    fixed js and css references

--- v2/index.php
+++ v2/index.php
@@ -4,33 +4,17 @@
 <!-- Sencha CSS -->
 <link rel="stylesheet" href="sencha/resources/css/ext-touch.css" type="text/css">
 <!-- Custom CSS -->
-<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="stylesheet" href="css/style.css" type="text/css">
 <!-- Ext Touch JS -->
 <script type="text/javascript" src="sencha/ext-touch-debug.js"> </script>
 <!-- Application JS -->
-<script type="text/javascript" src="index.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
 </head>
 <body>
 <div id="everything">
 <script>
 	document.write(STATE.getSavedNotes());
 </script>
-<?php
-// if ($handle = opendir('notes/')) {
-// 
-//     /* This is the correct way to loop over the directory. */
-//     while (false !== ($file = readdir($handle))) {
-//         $item = json_decode(file_get_contents('notes/' . $file), true);
-// 		if ($item) {
-// 			print "<div class=\"instance savedNote\" id=\"{$item['guid']}\" style=\"top:{$item['top']}px;left:{$item['left']}px;/*height:{$item['height']}px;*/\">";
-// 			print htmlentities($item['content']);
-// 			print "</div>\n";
-// 		}
-//     }
-// 
-// 
-//     closedir($handle);
-// }
-?></div>
+</div>
 </body>
 </html>
